e2644b10b26a10b6bc161a2392ef62dde1a4179a
codedhruv08 <dhruvgautam815@gmail.com>
2025-08-19 10:27:43 +0530

    Create Q1Harryage.java

---
+++ ProgrammingElementsPractise/Level1 PractiseProblem/Q1Harryage.java
@@ -0,0 +1,10 @@
+public class Q1Harryage {
+    public static void main(String[] args) {
+        int Birth_Year = 2000;
+        int Current_Year = 2024;
+
+        int Current_Age = Current_Year - Birth_Year;
+
+        System.out.println("Harry's age in 2024 is " + Current_Age);
+    }
+}
