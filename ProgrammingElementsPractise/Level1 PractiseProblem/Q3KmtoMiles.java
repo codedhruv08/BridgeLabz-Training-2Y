cf3dbd607c07236d199824dc86b5b2e14b6fd884
Dhruv Gautam <dhruvgautam815@gmail.com>
2025-09-15 22:29:10 +0530

    wiped main branch

--- ProgrammingElementsPractise/Level1 PractiseProblem/Q3KmtoMiles.java
+++
@@ -1,9 +0,0 @@
-public class Q3KmtoMiles {
-     public static void main(String[] args) {
-        double Km = 10.8;
-
-        double Miles = Km * 0.62;
-
-        System.out.println("The distance "+ Km +"in miles is "+ Miles);
-    }
-}
